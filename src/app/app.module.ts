ac9eade3a3cc1866de8dcdbb213b45c0e7140df5
Julia lopes <jlr.nwb@gmail.com>
2023-01-24 12:48:56 -0300

    Layout implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +12,9 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ExchangeService } from '../services/exchange.service';
+import { Symbol } from './symbol';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  currencyList: Symbol[] = [];
+
+  amount: number = 1.00;
+  currency1: String = "BRL";
+  currency2: String = "USD";
+  result?: number;
+
+  constructor(private exchangeService: ExchangeService) { }
 
   ngOnInit(): void {
+    this.getExchange();
+  }
+
+  getExchange() {
+    this.exchangeService.getSymbols().subscribe({
+      next: (res) =>  {
+        Object.values(res.symbols).map((s: any) => {
+          this.currencyList.push(new Symbol(s.description, s.code));
+        })
+      },
+      error: (err) =>  console.log(err)
+    })
+  }
+
+  changeCurrency() {
+    const temp = this.currency1;
+    this.currency1 = this.currency2;
+    this.currency2 = temp;
+    this.result = undefined;
+  }
+
+  convert() {
+    this.exchangeService.convert(this.currency1, this.currency2, this.amount).subscribe({
+      next: (res) =>  {
+        this.result = res.result;
+      },
+      error: (err) =>  console.log(err)
+    })
   }
 
 }
